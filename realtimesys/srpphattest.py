369058210bd8873980864a8acd91e99c6fa60627
twin <550847434@qq.com>
2021-03-19 15:05:32 +0800

    加窗，加noise bins全部去除，效果还是不行

--- realtimesys/srpphattest.py
+++ realtimesys/srpphattest.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.fftpack import rfft, irfft, fftfreq, fft, ifft, fftshift
 
+import scipy.signal as signal
+
 from audiotools.util import load_audio_data
 from dsptools.filter import butter_bandpass_filter
 from realtimesys.srpphat import get_steering_vector, cons_uca, plot_angspect, vec2theta
@@ -12,7 +14,6 @@ def power2db(power):
         raise ValueError("power less than 0")
     db = (10 * np.log10(power) + 300) - 300
     return db
-
 
 def normalized_signal_fft(data, fs=48e3, figure=True, xlim=(19e3, 21e3)):
     N = len(data)
@@ -70,20 +71,34 @@ def denoise_fft(target_fft, noise_fft):
     phase = np.angle(target_fft)
     f1 = np.abs(target_fft)
     f2 = np.abs(noise_fft)
+
     # f1 = target_fft
     # f2 = noise_fft
     doppler_fft_abs = f1 - f2
 
+
+
     doppler_fft_abs[doppler_fft_abs < 0] = 0
+
+
+    sorted_index = np.argsort(f2)[::-1]
+    bins = f2[sorted_index]
+    noise_bins_num = 8
+    doppler_fft_abs[sorted_index[:noise_bins_num]] = 0
+
+    # plt.plot(doppler_fft_abs)
+    # plt.show()
+
     # for i in range(doppler_fft_abs.shape[0]):
     #     normalized_signal_fft_with_fft(f1[0], 'target_fft')
     #     normalized_signal_fft_with_fft(f2[0], 'noise_fft')
     #     normalized_signal_fft_with_fft(doppler_fft_abs[i], 'doppler_fft')
     #     plt.show()
-    normalized_signal_fft_with_fft(f1, 'target_fft')
-    normalized_signal_fft_with_fft(f2, 'noise_fft')
-    normalized_signal_fft_with_fft(doppler_fft_abs, 'doppler_fft')
-    plt.show()
+
+    # normalized_signal_fft_with_fft(f1, 'target_fft')
+    # normalized_signal_fft_with_fft(f2, 'noise_fft')
+    # normalized_signal_fft_with_fft(doppler_fft_abs, 'doppler_fft')
+    # plt.show()
 
     doppler_fft = doppler_fft_abs * np.exp(1j*phase)
     return doppler_fft
@@ -114,7 +129,7 @@ def gcc_phat_search(x_i, x_j, fs, tau):
     return np.abs(R)
 
 
-def gcc_phat_search_fft_denoise(x_i, x_j, fs, tau, noise_fft_i, noise_fft_j):
+def gcc_phat_search_fft_denoise(x_i, x_j, fs, tau, noise_fft_i, noise_fft_j, window):
     """
     :param x_i: real signal of mic i
     :param x_j: real signal of mic j
@@ -122,8 +137,8 @@ def gcc_phat_search_fft_denoise(x_i, x_j, fs, tau, noise_fft_i, noise_fft_j):
     :return: np array, shape = (n_frames, num_of_search_grid)
     """
     # 这里对fft去噪声
-    fft_xi = fft(x_i)
-    fft_xj = fft(x_j)
+    fft_xi = fft(x_i * window)
+    fft_xj = fft(x_j * window)
 
     denoised_fft_xi = denoise_fft(fft_xi, noise_fft_i)
     denoised_fft_xj = denoise_fft(fft_xj, noise_fft_j)
@@ -141,6 +156,23 @@ def gcc_phat_search_fft_denoise(x_i, x_j, fs, tau, noise_fft_i, noise_fft_j):
     exp_part = np.outer(k, 2j * np.pi * tau * fs / num_bins)
     R = np.dot(A, np.exp(exp_part)) / num_bins
     return np.abs(R)
+
+def srp_phat_denoise(raw_signal, mic_array_pos, search_grid, c, fs, noise_fft, window):
+    assert raw_signal.shape[0] == mic_array_pos.shape[0]
+    mic_num = mic_array_pos.shape[0]
+    # grid, _ = create_spherical_grids(level)
+    # print(grid.shape)
+    E_d = np.zeros((1, search_grid.shape[0]))  # (num_frames, num_points), 之后要改
+    for i in range(mic_num):
+        for j in range(i + 1, mic_num):
+            # tau is given in second, 这个也可以提前计算
+            tau = get_steering_vector(mic_array_pos[i], mic_array_pos[j], c, search_grid)
+            # t1 = time.time()
+            R_ij = gcc_phat_search_fft_denoise(raw_signal[i], raw_signal[j], fs, tau, noise_fft[i], noise_fft[j], window)
+            # t2 = time.time()
+            E_d += R_ij
+            # print('each pair time consuption: ', t2 - t1)
+    return E_d
 
 # 用ifft和用search_grid做对比
 def compare():
@@ -204,7 +236,13 @@ def compare():
 
 # 谱减法去噪
 def fft_denoise_test():
+
     frame_len = 2048
+    # 加窗窗口
+    window = np.hanning(frame_len)
+    # window = np.ones(frame_len)
+    # window = np.hamming(frame_len)
+
     data, fs = load_audio_data(r'D:\projects\pyprojects\gesturerecord\location\20khz\0.wav', 'wav')
     data = data[fs * 1:, :-1].T
     data_filter = butter_bandpass_filter(data, 19e3, 21e3)
@@ -225,8 +263,8 @@ def fft_denoise_test():
     level = 4
     grid = np.load(rf'grid/{level}.npz')['grid']
     tau = get_steering_vector(mic_array_pos[i], mic_array_pos[j], c, grid)
-    noise_fft = fft(noise_data)
-    R = gcc_phat_search_fft_denoise(test_data[i], test_data[j], fs, tau, noise_fft[i], noise_fft[j])
+    noise_fft = fft(noise_data * window)
+    R = gcc_phat_search_fft_denoise(test_data[i], test_data[j], fs, tau, noise_fft[i], noise_fft[j], window)
 
     print('gccphat search max val: ', np.max(R))
     print('gccphat delay of sample num: ', tau[np.argmax(R)] * fs)
@@ -240,5 +278,43 @@ def fft_denoise_test():
     # plt.plot(np.correlate(a,b,'full'))
     plt.show()
 
+
+# 谱减法去噪
+def srp_fft_denoise_test():
+
+    frame_len = 2048
+    # 加窗窗口
+    window = np.hanning(frame_len)
+    # window = np.ones(frame_len)
+    # window = np.hamming(frame_len)
+
+    data, fs = load_audio_data(r'D:\projects\pyprojects\gesturerecord\location\20khz\0.wav', 'wav')
+    data = data[fs * 1:, :-1].T
+    data_filter = butter_bandpass_filter(data, 19e3, 21e3)
+
+    # for f in data_filter:
+    #     normalized_signal_fft(f)
+
+    t_fs_n = 40000
+    noise_data = data_filter[:, t_fs_n:t_fs_n+frame_len]
+    t_fs_t = 48000 * 5 + 1000
+    test_data = data_filter[:, t_fs_t:t_fs_t+frame_len]
+    # denoise_fft(fft(test_data)/frame_len, fft(noise_data)/frame_len)
+
+    mic_array_pos = cons_uca(0.043)
+    c = 343
+    level = 4
+    grid = np.load(rf'grid/{level}.npz')['grid']
+    noise_fft = fft(noise_data * window)
+    E = srp_phat_denoise(test_data, mic_array_pos, grid, c, fs, noise_fft, window)
+    sdevc = grid[np.argmax(E, axis=1)]  # source direction vector
+    # print(sdevc)
+    print('angle of  max val: ', np.rad2deg(vec2theta(sdevc)))
+    print('=' * 50)
+
 if __name__ == '__main__':
-    fft_denoise_test()
+    # fft_denoise_test()
+    srp_fft_denoise_test()
+    # hanning_window = np.hanning(2048)
+    # plt.plot(hanning_window)
+    # plt.show()
